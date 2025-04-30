faff0904f1eb5176b61d2b4e54cafabd78fc7bbf
NetNinjaEngineer <memo5260287@outlook.com>
2025-04-30 18:24:56 +0300

    #Updated CreateShortCommandValidator

--- GreenSphere.Application/Features/Shorts/Commands/CreateShort/CreateShortCommandValidator.cs
+++ GreenSphere.Application/Features/Shorts/Commands/CreateShort/CreateShortCommandValidator.cs
@@ -8,9 +8,20 @@ public sealed class CreateShortCommandValidator : AbstractValidator<CreateShortC
 {
     public CreateShortCommandValidator()
     {
+        RuleFor(x => x.Title)
+            .NotEmpty().WithMessage("Title is required.")
+            .MaximumLength(100).WithMessage("Title cannot exceed 100 characters.");
+
+        RuleFor(x => x.Description)
+            .MaximumLength(500).WithMessage("Description cannot exceed 500 characters.");
+
         RuleFor(c => c.Video)
             .Must(v => v.Length > 0).WithMessage("Select a short video file.")
             .Must(BeAvalidVideoFile).WithMessage("Not a video file.");
+
+        RuleFor(x => x.Thumbnail)
+            .Must(BeAValidImage).When(x => x.Thumbnail != null)
+            .WithMessage("Invalid thumbnail format. Only JPG, JPEG and PNG are allowed.");
 
     }
 
@@ -18,5 +29,11 @@ public sealed class CreateShortCommandValidator : AbstractValidator<CreateShortC
     {
         var extension = Path.GetExtension(videoFile.FileName).ToLower();
         return FileFormats.AllowedVideoFormats.Contains(extension);
+    }
+
+    private static bool BeAValidImage(IFormFile? file)
+    {
+        var extension = Path.GetExtension(file!.FileName).ToLower();
+        return FileFormats.AllowedImageFormats.Contains(extension);
     }
 }
